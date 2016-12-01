204d7225b1c5aed7ec386bc61da347e3cba31129
Pham Dinh Giang <giang.pham@dena.com>
2016-12-01 14:14:42 +0700

    Init my docker repo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# mydocker
+# COMMON DOCKER FILES
+
+### Init CentOS 7 image
+docker build --file centos-dockerfile -t mycentos:v1 ./
+
+### Init scrapy docker 
+docker-compose  -f scrapy-composer.yaml up
---
+++ scripts/install-pip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Install PIP
+source ~/.bashrc
+python --version
+curl https://bootstrap.pypa.io/get-pip.py | python
+
+# Install virtualenv
+pip install virtualenv
+
+# Install scrapy
+pip install Scrapy
+
+# Keep container alive
+tail -fn100 /dev/null
